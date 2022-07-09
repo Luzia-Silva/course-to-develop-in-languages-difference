9c7e0976c24be8f8f15d8bcd5d7644881fc3e5a1
Luzia <aluziagabriela@gmail.com>
2022-07-09 18:55:52 -0300

    projetos para aprender javascript avançado

---
+++ courseAlura/courseB7/test/my-app/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from '../../../../../my-app/src/styles'
+
+export const Activities: React.FC = () => {
+  return(
+      <>
+      <S.Container>
+        
+      </S.Container>
+      </>
+  )
+
+  };
---
+++ my-app/src/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+`;
+
+export const Content = styled.span`
+  display: flex;
+  background: #000;
+`;
